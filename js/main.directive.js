d48c2a3617b40f1b21c2a2460bf0cb5570113ae2
amin mesgari <mesgari.dev@gmail.com>
2016-10-15 12:25:53 +0330

    problem is test is like them

--- js/main.directive.js
+++ js/main.directive.js
@@ -1,16 +1,18 @@
 app.directive('itemsRepeat', function () {
     return {
-        restrict: 'E',
         templateUrl: 'item.html',
         scope: {
-            info: '='
+            items: '='
         },
         link: function ($scope) {
-            $scope.plus = function () {
-                this.info.like += 1;
-            };
-            $scope.minez = function () {
-                this.info.like -= 1;
+            $scope.plus = function(item) {
+
+                item.like ++;console.log(item)
+
+            }
+            $scope.mines = function(item) {
+                item.like --;
+                console.log(item)
             }
         }
     };
--- js/mainController.js
+++ js/mainController.js
@@ -1,36 +1,45 @@
 app.controller('mainController', ['$scope', function ($scope) {
     $scope.items = [
         {
-            text:'hi , this text is test',
+            id: 1,
+            text:'hi , this text is test id = 1',
             like : 0
         },
         {
-            text:'hi , this text is test',
+            id: 1,
+            text:'hi , this text is test id = 1',
             like : 0
         },
         {
-            text:'hi , this text is test',
+            id: 3,
+            text:'hi , this text is test id = 3',
             like : 1
         },
         {
-            text:'hi , this text is test',
+            id: 4,
+            text:'hi , this text is test id = 4',
             like : 0
         },
         {
-            text:'hi , this text is test',
+            id: 5,
+            text:'hi , this text is test id = 5',
+            like : 2
+        },
+        {
+            id: 6,
+            text:'hi , this text is test id = 6',
             like : 0
         },
         {
-            text:'hi , this text is test',
-            like : 0
+            id: 7,
+            text:'hi , this text is test id = 7',
+            like : 3
         },
         {
-            text:'hi , this text is test',
-            like : 0
-        },
-        {
-            text:'hi , this text is test',
+            id: 8,
+            text:'hi , this text is test id = 8',
             like : 0
         }
     ];
+    
 }]);
